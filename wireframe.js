99cb5e1d16cabe7e50f78e4db676df4da4e5a96f
Trevor Reynolds <trevor.reynolds@rmit.edu.au>
2018-07-11 20:16:07 +1000

    Wireframe.css update to toggle other stylesheets as well as wireframe.css

--- wireframe.js
+++ wireframe.js
@@ -1,4 +1,8 @@
 function toggleWireframe() {
   let wfs = document.getElementById("wireframecss");
+  let sts = document.getElementById("stylecss");
+  // repeat for other stylesheets you are linking to
   wfs.disabled = !wfs.disabled;
+  sts.disabled = !sts.disabled;
+  // repeat for other stylesheets you are linking to
 }
